37458c34ab9efa94f78428fd9a82aa01742ecd52
Kushan <www.kushanchamindu@gmail.com>
2020-03-26 12:46:35 +0530

    Pop up menu button done!!

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -6,12 +6,49 @@ import 'package:healthcaresupportsystem/CKD_Card/CKD_Card.dart';
 
 
 class Home extends StatelessWidget {
+
+  void choiceAction(String choice){
+    print('Working');
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.blue[100],
         appBar: AppBar(
           title: Text('Home'),
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              onSelected: choiceAction,
+              itemBuilder: (BuildContext context){
+                return Constant.choice.map((String choice){
+                  if(choice=='Account'){
+                  return PopupMenuItem(
+                    value: choice,
+                      child: Row(children: <Widget>[Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(Icons.person, color: Colors.black,),
+                      ), Text(choice)],));
+
+                }else if(choice=='SignOut'){
+                  return PopupMenuItem(
+                      value: choice,
+                      child: Row(children: <Widget>[Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(Icons.lock, color: Colors.black,),
+                      ), Text(choice)],));
+                }else{
+                  return PopupMenuItem(
+                      value: choice,
+                      child: Row(children: <Widget>[Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(Icons.flag, color: Colors.black,),
+                      ), Text(choice)],));
+                  }
+                }
+                ).toList();
+              },
+          )
+          ],
           centerTitle: true,
           backgroundColor: Colors.blueAccent,
           leading: Padding(
@@ -33,4 +70,15 @@ class Home extends StatelessWidget {
         )
     );
   }
+}
+class Constant{
+  static const String Account='Account';
+  static const String SignOut='SignOut';
+  static const String About='About';
+
+  static const List<String> choice=<String>[
+    Account,
+    SignOut,
+    About
+  ];
 }
