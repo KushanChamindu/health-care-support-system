f9d1c4ceccef489c154f6ad4c9304b4215d361a9
Kushan <www.kushanchamindu@gmail.com>
2020-03-26 11:48:08 +0530

    precautions done!!

--- lib/Pages/CKD_pages/CKD_precautions.dart
+++ lib/Pages/CKD_pages/CKD_precautions.dart
@@ -48,15 +48,99 @@ class _CKD_precautionsState extends State<CKD_precautions> {
 //                  ],
 //                ),
 //              )
-              Text('cat'),
-              Row(
-                children: <Widget>[
-                  Image.asset('assets/CKD_image/low_salt.jpg', width: 100,height: 100,),
-                      ],
+              Card(
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MyBullet(),
                     ),
-
+                    Expanded(child: Text('Follow a low-salt, low-fat diet',style: TextStyle(fontSize:17, fontWeight: FontWeight.w800) )),
+                    Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Image.asset('assets/CKD_image/fruit.png', width: 110,height: 100,),
+                    ),
+                        ],
+                      ),
+              ),
+              Card(
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MyBullet(),
+                    ),
+                    Expanded(child: Text('Exercise at least 30 minutes on most days of the week',style: TextStyle(fontSize:17, fontWeight: FontWeight.w800) )),
+                    Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Image.asset('assets/CKD_image/excersis.gif', width: 110,height: 100,),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MyBullet(),
+                    ),
+                    Expanded(child: Text('Have regular check-ups with your doctor',style: TextStyle(fontSize:17, fontWeight: FontWeight.w800) )),
+                    Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Image.asset('assets/CKD_image/meet_doctor.gif', width: 110,height: 100,),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MyBullet(),
+                    ),
+                    Expanded(child: Text('Do not smoke or use tobacco',style: TextStyle(fontSize:17, fontWeight: FontWeight.w800) )),
+                    Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Image.asset('assets/CKD_image/nosmoke.gif', width: 110,height: 100,),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MyBullet(),
+                    ),
+                    Expanded(child: Text('Limit alcohol',style: TextStyle(fontSize:17, fontWeight: FontWeight.w800) )),
+                    Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Image.asset('assets/CKD_image/noAlcohol.jpeg', width: 110,height: 100,),
+                    ),
+                  ],
+                ),
+              ),
                   ],
           ),
         ));
+  }
+}
+class MyBullet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(20, 8, 0, 3),
+      child: new Container(
+        height: 9.0,
+        width: 9.0,
+        decoration: new BoxDecoration(
+          color: Colors.black,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
   }
 }
