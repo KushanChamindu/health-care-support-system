fe39f0789f60c9c83bd5a4bb470d1f3501944f65
Kushan <ra.kushan1997@gmail.com>
2020-06-09 13:55:56 +0530

    minor changes

--- lib/Pages/breast_cancer/breast_solutions.dart
+++ lib/Pages/breast_cancer/breast_solutions.dart
@@ -55,6 +55,7 @@ class BC_Solutions extends StatelessWidget {
 //        backgroundColor: Colors.lightBlue,
       ),
        body:ListView.builder(
+           key: ValueKey('BCSolutionPage'),
            itemCount: precautions.length,
            itemBuilder: (context,index){
              return Card(
